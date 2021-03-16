1f3d301e621e6e9b1752eaa470f45c80736e232f
Sneha Mishra <itm2017004@iiita.ac.in>
2021-03-16 16:42:01 +0530

    add rothc example, swagger and upload own json for rothc/point

--- rest_api/app.py
+++ rest_api/app.py
@@ -1,16 +1,55 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request, jsonify
 from flask_restful import Resource, Api, reqparse
 import os
 import subprocess
 import time
+import json
 from datetime import datetime
+from flask_swagger import swagger
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 api = Api(app)
 
-@app.route('/help/<string:arg>')
+### swagger specific ###
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+	SWAGGER_URL,
+	API_URL,
+	config={
+		'app_name': "FLINT REST API"
+	}
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+### end swagger specific ###
+
+@app.route("/spec")
+def spec():
+	swag = swagger(app)
+	swag['info']['version'] = "1.0"
+	swag['info']['title'] = "FLINT Rest Api"
+	f = open("./static/swagger.json", "w+")
+	json.dump(swag,f)
+	return jsonify(swag)
+
+@app.route('/help/<string:arg>', methods=['GET'])
 def help(arg):
-	# return our data and 200 OK HTTP code
+	"""
+		Get Help Section
+		---
+		tags:
+		  - help
+		parameters:
+		- name: arg
+		  in: path
+		  description: Help info about named section. Pass all to get all info
+		  required: true
+		  type: string
+		responses:
+		  200:
+			description: Help
+		"""
 	s = time.time()
 	if arg=='all':
 		res = subprocess.run(['moja.cli', '--help'], stdout=subprocess.PIPE)
@@ -26,9 +65,17 @@ def help(arg):
 	return {'data': response}, 200
 
 
-@app.route('/version')
+@app.route('/version', methods=['GET'])
 def version():
-	# return our data and 200 OK HTTP code
+	"""
+		Get Version of FLINT
+		---
+		tags:
+		  - version
+		responses:
+		  200:
+			description: Version
+		"""
 	s = time.time()
 	res = subprocess.run(['moja.cli', '--version'], stdout=subprocess.PIPE)
 	e = time.time()
@@ -41,28 +88,66 @@ def version():
 	return {'data': response}, 200
 
 
-@app.route('/point')
+@app.route('/point', methods=['POST'])
 def point():
-	# return our data and 200 OK HTTP code
+	"""
+		Get Point example of FLINT
+		---
+		tags:
+		  - point
+		responses:
+		  200:
+			description: Point based example FLINT
+		"""
 	s = time.time()
-	point_example = 'config/point_example.json'
+	if 'file' in request.files:
+		uploaded_file = request.files['file']
+		if uploaded_file.filename != '':
+			uploaded_file.save('config/'+uploaded_file.filename)
+		point_example = 'config/'+uploaded_file.filename
+	else:
+		point_example = 'config/point_example.json'
 	lib_simple = 'config/libs.base.simple.json'
 	logging_debug = 'config/logging.debug_on.conf'
 
 	dateTimeObj = datetime.now()
 	timestampStr = dateTimeObj.strftime("%H:%M:%S.%f - %b %d %Y")
-	f = open("output_files/"+timestampStr + "point_example.csv", "w+")
+	f = open(timestampStr + "point_example.csv", "w+")
 	res = subprocess.run(['moja.cli', '--config', point_example, '--config', lib_simple, '--logging_config', logging_debug], stdout=f)
 	e = time.time()
 	UPLOAD_DIRECTORY = "./"	
-	'''
-	response = {
-		'exitCode' : res.returncode,
-		'execTime' : e - s,
-		'response' : send_from_directory(UPLOAD_DIRECTORY,"output_files/"+timestampStr + "point_example.csv", as_attachment=True)
-	}
-	'''
-	return send_from_directory(UPLOAD_DIRECTORY,"output_files/"+timestampStr + "point_example.csv", as_attachment=True), 200
+	return send_from_directory(UPLOAD_DIRECTORY,timestampStr + "point_example.csv", as_attachment=True), 200
+
+
+@app.route('/rothc', methods=['POST'])
+def rothc():
+	"""
+		Get RothC example of FLINT
+		---
+		tags:
+		  - rothc
+		responses:
+		  200:
+			description: RothC based example FLINT
+		"""
+	s = time.time()
+	if 'file' in request.files:
+		uploaded_file = request.files['file']
+		if uploaded_file.filename != '':
+			uploaded_file.save('config/'+uploaded_file.filename)
+		point_example = 'config/'+uploaded_file.filename
+	else:
+		point_example = 'config/point_rothc_example.json'
+	lib_simple = 'config/libs.base_rothc.simple.json'
+	logging_debug = 'config/logging.debug_on.conf'
+
+	dateTimeObj = datetime.now()
+	timestampStr = dateTimeObj.strftime("%H:%M:%S.%f - %b %d %Y")
+	f = open(timestampStr + "point_rothc_example.csv", "w+")
+	res = subprocess.run(['moja.cli', '--config', point_example, '--config', lib_simple, '--logging_config', logging_debug], stdout=f)
+	e = time.time()
+	UPLOAD_DIRECTORY = "./"	
+	return send_from_directory(UPLOAD_DIRECTORY,timestampStr + "point_rothc_example.csv", as_attachment=True), 200
 
 
 if __name__ == '__main__':
