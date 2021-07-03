040baebfc662e157b1e84cc73a62c6d76d03a5c7
Rajiv Singh <rajivperfect007@gmail.com>
2021-07-03 14:26:34 +0530

    Update app.py
    Signed-off-by: Rajiv Ranjan Singh <rajivperfect007@gmail.com>

--- rest_api_flint.example/app.py
+++ rest_api_flint.example/app.py
@@ -196,4 +196,4 @@ def rothc():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="127.0.0.1", port=int(os.environ.get("PORT", 8080)))
