e6b7b7413b5c68bc3f9731c791d1dbd0b2d40cf1
Rajiv Singh <rajivperfect007@gmail.com>
2021-07-03 14:26:57 +0530

    Update app.py
    Signed-off-by: Rajiv Ranjan Singh <rajivperfect007@gmail.com>

--- rest_api_gcbm/app.py
+++ rest_api_gcbm/app.py
@@ -586,4 +586,4 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="127.0.0.1", port=int(os.environ.get("PORT", 8080)))
