a8137a593369cf4cc9528d77d758aea1a74225ef
HarshCasper <erbeusgriffincasper@gmail.com>
2022-01-25 16:39:47 +0530

    CHORE: Format Terraform
    Signed-off-by: HarshCasper <erbeusgriffincasper@gmail.com>

--- layered/main.tf
+++ layered/main.tf
@@ -1,79 +1,79 @@
 # Modifiable variables
 variable "project" {
-  type = string
+  type    = string
   default = "flint-cloud"
 }
 
 variable "region" {
-  type = string
+  type    = string
   default = "us-central1"
 }
 
 variable "zone" {
-  type = string
+  type    = string
   default = "us-central1-a"
 }
 
 variable "ingress_name" {
-  type = string
+  type    = string
   default = "fc-ingress"
 }
 
 variable "cr_processor_name" {
-  type = string
+  type    = string
   default = "fc-cr-processor"
 }
 
 variable "gce_processor_name" {
-  type = string
+  type    = string
   default = "fc-gce-processor"
 }
 
 variable "gce_processor_cpu" {
-  type = number
+  type    = number
   default = 4
 }
 
 variable "gce_processor_memory" {
-  type = number
+  type    = number
   default = 10240
 }
 
 variable "gce_processor_disk" {
-  type = number
+  type    = number
   default = 15
 }
 
 # Project config
 provider "google" {
   credentials = file("service_account.json")
-  project = var.project
-  region = var.region
+  project     = var.project
+  region      = var.region
 }
 
 # Enable APIs
 resource "google_project_service" "run" {
-  service = "run.googleapis.com"
+  service            = "run.googleapis.com"
   disable_on_destroy = false
 }
 
 resource "google_project_service" "pubsub" {
-  service = "pubsub.googleapis.com"
+  service            = "pubsub.googleapis.com"
   disable_on_destroy = false
 }
 
 resource "google_project_service" "storage" {
-  service = "storage-component.googleapis.com"
+  service            = "storage-component.googleapis.com"
   disable_on_destroy = false
 }
 
 resource "google_project_service" "compute" {
-  service = "compute.googleapis.com"
+  service            = "compute.googleapis.com"
   disable_on_destroy = false
 }
 
 resource "google_project_service" "iam" {
-  service = "iam.googleapis.com"
+  service            = "iam.googleapis.com"
   disable_on_destroy = false
 }
 
@@ -81,7 +81,7 @@ resource "google_project_service" "iam" {
 resource "google_service_account" "fc-sa" {
   account_id   = "flint-cloud-sa"
   display_name = "FLINT Cloud Service Account"
-  project = var.project
+  project      = var.project
 
   depends_on = [google_project_service.iam]
 }
@@ -111,7 +111,7 @@ resource "google_project_iam_member" "fc-sa-binding-compute" {
 }
 
 resource "google_service_account_key" "fc-sa-key" {
-  service_account_id = "${google_service_account.fc-sa.id}"
+  service_account_id = google_service_account.fc-sa.id
 
   depends_on = [google_service_account.fc-sa]
 }
@@ -131,43 +131,43 @@ resource "google_pubsub_topic" "large-simulations" {
 
 # Ingress service
 resource "google_cloud_run_service" "fc-ingress" {
-  name = var.ingress_name
+  name     = var.ingress_name
   location = var.region
-  
+
   template {
     spec {
       containers {
         image = "gcr.io/flint-cloud/ingress:latest"
         env {
-          name = "PROJECT_NAME"
+          name  = "PROJECT_NAME"
           value = var.project
         }
         env {
-          name = "GCE_ZONE"
+          name  = "GCE_ZONE"
           value = var.zone
         }
         env {
-          name = "GCE_NAME"
+          name  = "GCE_NAME"
           value = var.gce_processor_name
         }
         env {
-          name = "SERVICE_ACCOUNT"
-          value = "${base64decode(google_service_account_key.fc-sa-key.private_key)}"
+          name  = "SERVICE_ACCOUNT"
+          value = base64decode(google_service_account_key.fc-sa-key.private_key)
         }
       }
     }
   }
 
   traffic {
-    percent = 100
+    percent         = 100
     latest_revision = true
   }
   autogenerate_revision_name = true
-  depends_on = [google_project_service.run, google_service_account_key.fc-sa-key]
+  depends_on                 = [google_project_service.run, google_service_account_key.fc-sa-key]
 }
 
 output "ingress-url" {
-  value = "${google_cloud_run_service.fc-ingress.status[0].url}"
+  value = google_cloud_run_service.fc-ingress.status[0].url
 }
 
 data "google_iam_policy" "noauth-ingress" {
@@ -188,9 +188,9 @@ resource "google_cloud_run_service_iam_policy" "noauth-ingress" {
 
 # Cloud Run processor
 resource "google_cloud_run_service" "fc-cr-processor" {
-  name = var.cr_processor_name
+  name     = var.cr_processor_name
   location = var.region
-  
+
   template {
     spec {
       containers {
@@ -202,29 +202,29 @@ resource "google_cloud_run_service" "fc-cr-processor" {
           }
         }
         env {
-          name = "LD_LIBRARY_PATH"
+          name  = "LD_LIBRARY_PATH"
           value = "/opt/gcbm:/usr/local//lib:/usr/local//lib/x86_64-linux-gnu:/usr/local/lib:/usr/local/lib/x86_64-linux-gnu:/usr/local/lib:"
         }
         env {
-          name = "PROJECT_NAME"
+          name  = "PROJECT_NAME"
           value = var.project
         }
         env {
-          name = "SERVICE_ACCOUNT"
-          value = "${base64decode(google_service_account_key.fc-sa-key.private_key)}"
+          name  = "SERVICE_ACCOUNT"
+          value = base64decode(google_service_account_key.fc-sa-key.private_key)
         }
       }
-      timeout_seconds = 3599
+      timeout_seconds       = 3599
       container_concurrency = 1
     }
   }
 
   traffic {
-    percent = 100
+    percent         = 100
     latest_revision = true
   }
   autogenerate_revision_name = true
-  depends_on = [google_project_service.run, google_service_account_key.fc-sa-key]
+  depends_on                 = [google_project_service.run, google_service_account_key.fc-sa-key]
 }
 
 data "google_iam_policy" "noauth-cr-processor" {
@@ -244,12 +244,12 @@ resource "google_cloud_run_service_iam_policy" "noauth-cr-processor" {
 }
 
 resource "google_pubsub_subscription" "small-simulations-sub" {
-  name  = "small-simulations-sub"
-  topic = google_pubsub_topic.small-simulations.name
+  name                 = "small-simulations-sub"
+  topic                = google_pubsub_topic.small-simulations.name
   ack_deadline_seconds = 10
 
   push_config {
-    push_endpoint = "${google_cloud_run_service.fc-cr-processor.status[0].url}"
+    push_endpoint = google_cloud_run_service.fc-cr-processor.status[0].url
   }
 
   depends_on = [google_pubsub_topic.small-simulations, google_cloud_run_service.fc-cr-processor]
@@ -257,8 +257,8 @@ resource "google_pubsub_subscription" "small-simulations-sub" {
 
 # GCS bucket
 resource "google_storage_bucket" "simulation_data_flint-cloud" {
-  name = "simulation_data_flint-cloud"
-  location = var.region
+  name          = "simulation_data_flint-cloud"
+  location      = var.region
   force_destroy = true
 
   depends_on = [google_project_service.storage]
@@ -266,15 +266,15 @@ resource "google_storage_bucket" "simulation_data_flint-cloud" {
 
 # Compute Engine processor
 resource "google_pubsub_subscription" "large-simulations-sub" {
-  name  = "large-simulations-sub"
-  topic = google_pubsub_topic.large-simulations.name
+  name                 = "large-simulations-sub"
+  topic                = google_pubsub_topic.large-simulations.name
   ack_deadline_seconds = 10
 
   depends_on = [google_pubsub_topic.large-simulations]
 }
 
 module "gce-container" {
-  source = "terraform-google-modules/container-vm/google"
+  source  = "terraform-google-modules/container-vm/google"
   version = "~> 2.0"
 
   container = {
@@ -284,19 +284,19 @@ module "gce-container" {
     }
     env = [
       {
-        name = "PROJECT_NAME"
+        name  = "PROJECT_NAME"
         value = var.project
       },
       {
-        name = "GCE_ZONE"
+        name  = "GCE_ZONE"
         value = var.zone
       },
       {
-        name = "GCE_NAME"
+        name  = "GCE_NAME"
         value = var.gce_processor_name
       },
       {
-        name = "SERVICE_ACCOUNT"
+        name  = "SERVICE_ACCOUNT"
         value = "${base64decode(google_service_account_key.fc-sa-key.private_key)}"
       }
     ]
@@ -308,33 +308,33 @@ module "gce-container" {
 }
 
 resource "google_compute_instance" "fc-gce-processor" {
-  name = var.gce_processor_name
-  project = var.project
+  name         = var.gce_processor_name
+  project      = var.project
   machine_type = "custom-${var.gce_processor_cpu}-${var.gce_processor_memory}"
-  zone = var.zone
+  zone         = var.zone
 
   boot_disk {
     initialize_params {
       image = module.gce-container.source_image
-      size = var.gce_processor_disk
+      size  = var.gce_processor_disk
     }
   }
 
   network_interface {
-    network = "default"
+    network    = "default"
     subnetwork = "default"
     access_config {}
   }
 
   metadata = {
     gce-container-declaration = module.gce-container.metadata_value
-    google-logging-enabled = "true"
+    google-logging-enabled    = "true"
     google-monitoring-enabled = "true"
   }
 
   scheduling {
     automatic_restart = false
-    preemptible = false
+    preemptible       = false
   }
 
   service_account {
