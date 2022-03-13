f5b8bd9806494de0c9ca730b19473ff7dfbb2520
Harsh Mishra <erbeusgriffincasper@gmail.com>
2022-03-13 12:14:37 +0530

    Update test_rest_gcbm.py

--- local/tests/test_rest_gcbm.py
+++ local/tests/test_rest_gcbm.py
@@ -15,14 +15,13 @@ def create_demo_files():
     path_parent = os.path.dirname(path_parent)
     os.chdir(path_parent)
     print("path_parent ", path_parent)
-    zip_dir = os.getcwd() + '/GCBM_Demo_Run.zip'
-    test_files_dir = os.path.dirname(os.path.realpath(__file__)) + '/tests_files'
-    unzipped_file = zipfile.ZipFile(zip_dir, 'r')
+    zip_dir = os.getcwd() + "/GCBM_Demo_Run.zip"
+    test_files_dir = os.path.dirname(os.path.realpath(__file__)) + "/tests_files"
+    unzipped_file = zipfile.ZipFile(zip_dir, "r")
     unzipped_file.extractall(test_files_dir)
 
 
 class TestApiFlintGCBM:
-
     @pytest.fixture
     def base_endpoint(self):
         """ Port 8080 of the docker container is mapped to 8080 on the \
@@ -32,34 +31,50 @@ class TestApiFlintGCBM:
 
     @pytest.fixture
     def help_endpoint(self):
-        """ This is the endpoint for the help page """
+        """This is the endpoint for the help page"""
         self.ENDPOINT = "http://localhost:8080/help/"
         yield self.ENDPOINT
 
     @pytest.fixture
     def gcbm_endpoint(self):
-        """ This is the endpoint for the gcbm action """
+        """This is the endpoint for the gcbm action"""
         self.ENDPOINT = "http://localhost:8080/gcbm/"
         yield self.ENDPOINT
 
     @pytest.fixture
     def yield_title(self):
-        """ This would yield a title for the test """
+        """This would yield a title for the test"""
         yield "testtitle"
 
     @pytest.fixture
     def yield_config_files(self):
         """ This fixture yields a list of config files to be uploaded to the server via \
             gcbm/upload endpoint. Check the local_run.postman_collection in local/rest_api_gcbm """
-        config_files_dir = os.path.dirname(os.path.realpath(__file__)) + '/tests_files/config'
+        config_files_dir = (
+            os.path.dirname(os.path.realpath(__file__)) + "/tests_files/config"
+        )
         files = []
 
         for file in os.listdir(config_files_dir):
             if file.endswith(".json"):
-                temp = ('config_files', (file, open(config_files_dir + '/' + file, 'rb'), 'application/json'))
+                temp = (
+                    "config_files",
+                    (
+                        file,
+                        open(config_files_dir + "/" + file, "rb"),
+                        "application/json",
+                    ),
+                )
                 files.append(temp)
             elif file.endswith(".cfg"):
-                temp = ('config_files', (file, open(config_files_dir + '/' + file, 'rb'), 'application/octet-stream'))
+                temp = (
+                    "config_files",
+                    (
+                        file,
+                        open(config_files_dir + "/" + file, "rb"),
+                        "application/octet-stream",
+                    ),
+                )
                 files.append(temp)
 
         yield files
@@ -70,15 +85,27 @@ class TestApiFlintGCBM:
     def yield_input_files(self):
         """ This fixture yields a list of input files to be uploaded to the server via \
             gcbm/upload endpoint. Check the local_run.postman_collection in local/rest_api_gcbm """
-        input_files_dir = os.path.dirname(os.path.realpath(__file__)) + '/tests_files/layers/tiled'
+        input_files_dir = (
+            os.path.dirname(os.path.realpath(__file__)) + "/tests_files/layers/tiled"
+        )
         files = []
 
         for file in os.listdir(input_files_dir):
             if file.endswith(".tiff"):
-                temp = ('input', (file, open(input_files_dir + '/' + file, 'rb'), 'image/tiff'))
+                temp = (
+                    "input",
+                    (file, open(input_files_dir + "/" + file, "rb"), "image/tiff"),
+                )
                 files.append(temp)
             elif file.endswith(".json"):
-                temp = ('input', (file, open(input_files_dir + '/' + file, 'rb'), 'application/json'))
+                temp = (
+                    "input",
+                    (
+                        file,
+                        open(input_files_dir + "/" + file, "rb"),
+                        "application/json",
+                    ),
+                )
                 files.append(temp)
 
         yield files
@@ -87,18 +114,23 @@ class TestApiFlintGCBM:
     def yield_db_file(self):
         """ This fixture yields a db file to be uploaded to the server via \
             gcbm/upload endpoint. Check the local_run.postman_collection in local/rest_api_gcbm """
-        db_dir = os.path.dirname(os.path.realpath(__file__)) + '/tests_files/input_database'
+        db_dir = (
+            os.path.dirname(os.path.realpath(__file__)) + "/tests_files/input_database"
+        )
         files = []
 
         for file in os.listdir(db_dir):
             if file.endswith(".db"):
-                temp = ('db', (file, open(db_dir + '/' + file, 'rb'), 'application/octet-stream'))
+                temp = (
+                    "db",
+                    (file, open(db_dir + "/" + file, "rb"), "application/octet-stream"),
+                )
                 files.append(temp)
 
         yield files
 
     def test_check(self, base_endpoint):
-        """ This would test the base_endpoint """
+        """This would test the base_endpoint"""
         check_endpoint = base_endpoint + "check"
         check_response = requests.get(check_endpoint)
         assert check_response.status_code == 200
@@ -110,41 +142,42 @@ class TestApiFlintGCBM:
         spec_endpoint = base_endpoint + "spec"
         spec_response = requests.get(spec_endpoint)
         assert spec_response.status_code == 500
-    
+
     @pytest.mark.skip(reason="Test fails on CI")
     def test_list(self, gcbm_endpoint):
-        """ This would test the list endpoint """
+        """This would test the list endpoint"""
         list_endpoint = gcbm_endpoint + "list"
         list_response = requests.get(list_endpoint)
         assert list_response.status_code == 200
 
     def test_version(self, base_endpoint):
-        """ This would test the version endpoint """
+        """This would test the version endpoint"""
         version_endpoint = base_endpoint + "version"
         version_response = requests.get(version_endpoint)
         assert version_response.status_code == 200
 
     def test_help_all(self, help_endpoint):
-        """ This would test the help endpoint with 'all' argument """
+        """This would test the help endpoint with 'all' argument"""
         help_endpoint = help_endpoint + "all"
         help_response = requests.get(help_endpoint)
         assert help_response.status_code == 200
 
     def test_help_random_arg(self, help_endpoint):
-        """ This would test the help endpoint with a random argument """
-        random_arg = 'test'
+        """This would test the help endpoint with a random argument"""
+        random_arg = "test"
         help_endpoint = help_endpoint + random_arg
         help_response = requests.get(help_endpoint)
         assert help_response.status_code == 200
-        assert help_response.json()["data"]["response"] == f"Unknown section '{random_arg}' in the --help-section option\n"
+        assert (
+            help_response.json()["data"]["response"]
+            == f"Unknown section '{random_arg}' in the --help-section option\n"
+        )
 
     def test_new(self, gcbm_endpoint, yield_title):
         """ This test would create a new simulation under \
             the title provided by the yield_title fixture. """
         new_endpoint = gcbm_endpoint + "new"
-        data = {
-            'title': yield_title
-        }
+        data = {"title": yield_title}
         new_response = requests.post(new_endpoint, data=data)
         assert new_response.status_code == 200
 
@@ -152,7 +185,7 @@ class TestApiFlintGCBM:
         """ This test would try to give false positive on \
             uploading without any config files """
         data = {
-            'title': yield_title,
+            "title": yield_title,
         }
         files = []
         upload_endpoint = gcbm_endpoint + "upload"
@@ -164,31 +197,44 @@ class TestApiFlintGCBM:
         """ This test would try to give false positive on \
             uploading without any input files but with config files """
         data = {
-            'title': yield_title,
+            "title": yield_title,
         }
         upload_endpoint = gcbm_endpoint + "upload"
-        upload_response = requests.post(upload_endpoint, files=yield_config_files, data=data)
+        upload_response = requests.post(
+            upload_endpoint, files=yield_config_files, data=data
+        )
         assert upload_response.status_code == 400
         assert upload_response.json().get("error") == "Missing input"
 
-    def test_missing_database(self, yield_title, gcbm_endpoint, yield_config_files, yield_input_files):
+    def test_missing_database(
+        self, yield_title, gcbm_endpoint, yield_config_files, yield_input_files
+    ):
         """ This test would try to give false positive on \
             uploading without any database file but with config and input files """
         data = {
-            'title': yield_title,
+            "title": yield_title,
         }
         upload_endpoint = gcbm_endpoint + "upload"
-        upload_response = requests.post(upload_endpoint, files=yield_config_files + yield_input_files, data=data)
+        upload_response = requests.post(
+            upload_endpoint, files=yield_config_files + yield_input_files, data=data
+        )
         assert upload_response.status_code == 400
         assert upload_response.json().get("error") == "Missing database"
 
-    def test_upload(self, yield_title, yield_config_files, yield_input_files, yield_db_file, gcbm_endpoint):
+    def test_upload(
+        self,
+        yield_title,
+        yield_config_files,
+        yield_input_files,
+        yield_db_file,
+        gcbm_endpoint,
+    ):
         """ This test would upload the files provided by the \
             yield_config_files, yield_input_files and yield_db_file fixtures. \
             These files are needed for GCBM Implementation FLINT. """
         upload_files = yield_config_files + yield_input_files + yield_db_file
         data = {
-            'title': yield_title,
+            "title": yield_title,
         }
         upload_endpoint = gcbm_endpoint + "upload"
         upload_response = requests.post(upload_endpoint, files=upload_files, data=data)
@@ -201,7 +247,7 @@ class TestApiFlintGCBM:
             Dynamic Implementation of FLINT under the title provided \
             by yield_title fixture and already uploaded files."""
         data = {
-            'title': yield_title,
+            "title": yield_title,
         }
         dynamic_endpoint = gcbm_endpoint + "dynamic"
         dynamic_response = requests.post(dynamic_endpoint, data=data)
@@ -215,9 +261,7 @@ class TestApiFlintGCBM:
             we already uploaded the files under the title by \
             yield_title fixutre. """
         status_endpoint = gcbm_endpoint + "status"
-        data = {
-            "title": yield_title
-        }
+        data = {"title": yield_title}
         status_response = requests.post(status_endpoint, json=data)
         assert status_response.status_code == 200
         assert status_response.json().get("finished") == "In Progress"
@@ -229,7 +273,7 @@ class TestApiFlintGCBM:
             run the GCBM Implementation FLINT and we would have been got the output in the \
             input/yield_title/output.zip ."""
         data = {
-            'title': yield_title,
+            "title": yield_title,
         }
         download_endpoint = gcbm_endpoint + "download"
         download_response = requests.post(download_endpoint, data=data)
