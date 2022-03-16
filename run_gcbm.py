fcdb3610265868d09dabcb21b1aee61e9431b15d
Harikishan <harikishantk@gmail.com>
2022-03-16 19:38:08 +0530

    extract zip file implemented

--- run_gcbm.py
+++ run_gcbm.py
@@ -4,8 +4,13 @@
 # Show the output in nice visual way
 
 #import subprocess
-
-
+import zipfile
 import os
-os.system('cd local/rest_api_gcbm/ && docker-compose up -d')
 
+os.system('cd local/rest_api_gcbm/ && docker-compose up -d')
+zip_dir = os.path.dirname(os.path.realpath(__file__)) + "/GCBM_Demo_Run.zip"
+to_extract_path = os.path.dirname(os.path.realpath(__file__)) + "/GCBM_Demo_Run"
+if not os.path.isfile(to_extract_path):
+    os.mkdir(os.path.dirname(os.path.realpath(__file__)) + "/GCBM_Demo_Run")
+unzipped_file = zipfile.ZipFile(zip_dir, "r")
+unzipped_file.extractall(to_extract_path)
