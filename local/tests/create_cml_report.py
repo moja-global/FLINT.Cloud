bf6cd6a4878c6aebf889bc37d7899885230d5091
radis toubalidis <rtoumpalidis@gmail.com>
2022-04-11 23:28:24 +0300

    setup GCBM summary

--- local/tests/create_cml_report.py
+++ local/tests/create_cml_report.py
@@ -1,3 +1,4 @@
+from operator import index
 import os
 import pandas as pd
 
@@ -7,7 +8,7 @@ inputs = os.listdir(os.path.join(GCBM_template_path,'layers','tiled'))
 api_inputs = pd.DataFrame({"Data Files":[os.path.join('@','input','simulation_name',file) for file in inputs]})
 configs = os.listdir(os.path.join(GCBM_template_path,'config'))
 api_configs = pd.DataFrame({"Config files":[os.path.join('@','input','simulation_name',file) for file in configs]})
+md = pd.concat([api_inputs,api_configs])
 with open(os.path.join('..','rest_api_gcbm',"GCBM_summary.txt"),'w') as outfile:
-    outfile.writelines(api_inputs.to_markdown())
-    outfile.writelines(api_configs.to_markdown())
+    outfile.write(md.to_markdown(index=False))
     
