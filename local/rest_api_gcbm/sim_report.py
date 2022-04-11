645c9ba704f437739599c9518bca32370f6fb104
radis toubalidis <rtoumpalidis@gmail.com>
2022-04-11 19:49:09 +0300

    test if cml is setup right

---
+++ local/rest_api_gcbm/sim_report.py
@@ -0,0 +1,24 @@
+import os
+import pandas as pd
+
+TEST_ROOT = "./local/rest_api_gcbm"
+file_paths = os.path.join(TEST_ROOT,'input')
+simulations = os.listdir(file_paths)
+simulation_paths = [os.path.join(file_paths,s) for s in simulations]
+
+for sim in simulation_paths:
+    input_files = pd.DataFrame({"Input Files":[os.path.join(sim,match) for match in os.listdir(sim) if ".tiff" in match]})
+    config_files = pd.DataFrame({"Config Files":[os.path.join(sim,match) for match in os.listdir(sim) if "config" in match]})
+    logs = pd.DataFrame({"Log Files":[os.path.join(sim,match) for match in os.listdir(sim) if "log" in match]})
+    output = os.path.join(sim,'output')
+    if os.path.exists(output):
+        output_msg = "Output files located in " + output
+    else:
+        output_msg = "We have to run the simulation first check README.md"
+    sim_name = os.path.basename(sim)
+    s = {"Input Files":input_files,"Config files":config_files,"Logs":logs,"Output":output_msg}
+    with open(f"{sim_name}_summary.txt","w") as outfile:
+        outfile.write(f"## Sim name:{sim_name}\n\n")
+        outfile.write(input_files.to_markdown()+"\n\n")
+        outfile.write(config_files.to_markdown()+"\n\n")
+        outfile.write(logs.to_markdown())
