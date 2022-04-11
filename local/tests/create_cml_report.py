d109e1ee2974aca6dea3852d0a9e6b34d677e0f8
radis toubalidis <rtoumpalidis@gmail.com>
2022-04-11 23:09:21 +0300

    setup GCBM summary

--- local/tests/create_cml_report.py
+++ local/tests/create_cml_report.py
@@ -6,11 +6,8 @@ docker_path = os.path.join('app','input','your_sim_name')
 inputs = os.listdir(os.path.join(GCBM_template_path,'layers','tiled'))
 api_inputs = pd.DataFrame({"Data Files":[os.path.join('@','input','simulation_name',file) for file in inputs]})
 configs = os.listdir(os.path.join(GCBM_template_path,'config'))
-api_configs = pd.DataFrame({"Config files":[os.path.join('@','input','simulation_name',file) for file in os.listdir(configs)]})
-with open("GCBM_summary.txt",'w') as outfile:
-    outfile.write("## GCBM Model"+'\n')
-    outfile.write("### After your reproduce the GCBM Demo Run you docker image will have this structure"+'\n')
+api_configs = pd.DataFrame({"Config files":[os.path.join('@','input','simulation_name',file) for file in configs]})
+with open(os.path.join('..','rest_api_gcbm',"GCBM_summary.txt"),'w') as outfile:
     outfile.write(api_inputs.to_markdown()+"\n\n")
     outfile.write(api_configs.to_markdown()+"\n\n")
-    outfile.write(f"your output configuration will be in {os.path.join('@','input','output.zip')}")
     
