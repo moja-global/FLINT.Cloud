0550ea1caf83a10a977d7b8286132b96631bbab2
radis toubalidis <rtoumpalidis@gmail.com>
2022-05-25 19:13:26 +0300

    add sim start and end dates

--- local/rest_api_gcbm/cml_report/create_cml_report.py
+++ local/rest_api_gcbm/cml_report/create_cml_report.py
@@ -1,8 +1,10 @@
 from genericpath import isdir
+from operator import index
 import os
 import pandas as pd
 import tabulate
 import zipfile
+import json
 
 #  Unzip file
 zip = os.path.join('..','..','..','GCBM_Demo_Run.zip')
@@ -16,7 +18,14 @@ api_inputs = pd.DataFrame({"Data Files":inputs})
 configs = os.listdir(os.path.join(GCBM_template_path,'config'))
 api_configs = pd.DataFrame({"Config files":configs})
 output_config = f"Your output configuration will be in ```output.zip``` file which consists of the ```gcbm_output.db``` database and a directory for each metric calculated during the simulation."
-with open(os.path.join('GCBM_summary.txt'),'w') as outfile:
+
+# Simulation Information
+with open(os.path.join(GCBM_template_path,'config','localdomain.json')) as ld:
+    localdomain = json.load(ld)
+info = f" - Simulation Start Date : {localdomain['LocalDomain']['start_date']}\n - Simulation End Date : {localdomain['LocalDomain']['end_date']}"
+
+with open('GCBM_summary.txt','w') as outfile:
     outfile.write(api_inputs.to_markdown(index=False)+'\n\n')
     outfile.write(api_configs.to_markdown(index=False)+'\n\n')
-    outfile.write(output_config)
+    outfile.write(output_config+'\n\n')
+    outfile.write(info)
