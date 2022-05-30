68a3225285edfd565e61a951eef974410dd3af51
AayushSaini101 <kumaraayush9810@gmail.com>
2022-05-30 17:43:12 +0530

    Added testcases for endpoint
    Signed-off-by: AayushSaini101 <kumaraayush9810@gmail.com>

--- local/tests/test_rest_example.py
+++ local/tests/test_rest_example.py
@@ -21,3 +21,23 @@ class TestApiFlintExample:
         version_endpoint = base_endpoint + "version"
         version_response = requests.get(version_endpoint)
         assert version_response.status_code == 200
+
+    def test_help_all(self, base_endpoint):
+        """This would test the help endpoint with 'all' argument"""
+        base_endpoint = base_endpoint + "all"
+        base_response = request.get(base_endpoint)
+        assert base_response.status_code == 200
+
+    def test_help_random_arg(self, base_endpoint):
+        """This would test the help endpoint with a random argument"""
+        random_arg = "cli"
+        base_endpoint = base_endpoint + random_arg
+        base_response = requests.get(base_endpoint)
+        assert help_response.status_code == 200
+        assert (
+            help_response.json()["data"]["response"]
+            == f"Unknown section '{random_arg}' in the --help-section option\n"
+        )
+
+    
+
