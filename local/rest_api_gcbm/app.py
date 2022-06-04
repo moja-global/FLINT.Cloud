427b2ab16795c3ab87f5acb99e88eb074d709bb5
AayushSaini101 <kumaraayush9810@gmail.com>
2022-06-04 16:08:25 +0530

    Resolved the issue
    Signed-off-by: AayushSaini101 <kumaraayush9810@gmail.com>

--- local/rest_api_gcbm/app.py
+++ local/rest_api_gcbm/app.py
@@ -342,11 +342,12 @@ def gcbm_download():
     # Sanitize title
     title = "".join(c for c in title if c.isalnum())
     project_dir = f"{title}"
+    if not os.path.exists(f"{os.getcwd()}/input/{project_dir}"):
+        return {"error": "No such simulation exist for this title"}, 400
     return send_file(
         f"{os.getcwd()}/input/{project_dir}/output.zip",
         attachment_filename="output.zip",
     )
-
 
 @app.route("/gcbm/list", methods=["GET"])
 def gcbm_list_simulations():
