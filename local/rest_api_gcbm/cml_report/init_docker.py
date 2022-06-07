9ea2b355a62d16e2b2ca65abee515cc7d9b7a6de
radis toubalidis <rtoumpalidis@gmail.com>
2022-06-07 17:28:26 +0300

    run api and get list of saved simulations

--- local/rest_api_gcbm/cml_report/init_docker.py
+++ local/rest_api_gcbm/cml_report/init_docker.py
@@ -1,5 +1,5 @@
 import os
 
 os.system("docker build --build-arg BUILD_TYPE=RELEASE --build-arg NUM_CPU=4 -t gcbm-api .")
-os.system("docker run --rm -p 8080:8080 gcbm-api")
+os.system("docker run -d --rm -p 8080:8080 gcbm-api")
 
