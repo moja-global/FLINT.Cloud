5da627ebd6069e5770555940bed3684f9fbba1dd
radis toubalidis <rtoumpalidis@gmail.com>
2022-06-09 13:55:22 +0300

    check if sim run right

--- local/rest_api_gcbm/cml_report/create_cml_report.py
+++ local/rest_api_gcbm/cml_report/create_cml_report.py
@@ -8,7 +8,7 @@ import zipfile
 
 #  Unzip file
 zip = 'GCBM_Demo_Run.zip'
-with zipfile.ZipFile(zip,'r') as zip_ref:
+with zipfile.ZipFile(zip, 'r') as zip_ref:
     zip_ref.extractall('GCBM')
 
 # # Define input template as dataframes
--- local/rest_api_gcbm/cml_report/process_output.py
+++ local/rest_api_gcbm/cml_report/process_output.py
@@ -2,5 +2,5 @@ import os
 import zipfile
 
 output_zip = 'output.zip'
-with zipfile.ZipFile(output_zip,'r') as zip_ref:
+with zipfile.ZipFile(output_zip, 'r') as zip_ref:
     zip_ref.extractall('output')
