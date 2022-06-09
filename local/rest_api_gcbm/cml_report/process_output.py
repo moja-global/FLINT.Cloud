33586e12ce6c025fe1460660520b7dd6a136638d
radis toubalidis <rtoumpalidis@gmail.com>
2022-06-09 14:07:33 +0300

    check if sim run right

--- local/rest_api_gcbm/cml_report/process_output.py
+++ local/rest_api_gcbm/cml_report/process_output.py
@@ -2,5 +2,6 @@ import os
 import zipfile
 
 output_zip = 'output.zip'
-with zipfile.ZipFile(output_zip, 'r') as zip_ref:
-    zip_ref.extractall('output')
+if zipfile.is_zipfile(output_zip):
+    with zipfile.ZipFile(output_zip, 'r') as zip_ref:
+        zip_ref.extractall('output')
