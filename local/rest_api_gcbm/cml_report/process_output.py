ce105e1a02b87461e33c09aef50770267ffdc16f
radis toubalidis <rtoumpalidis@gmail.com>
2022-06-09 18:34:31 +0300

    check if sim runs using python script

--- local/rest_api_gcbm/cml_report/process_output.py
+++
@@ -1,7 +0,0 @@
-import os
-import zipfile
-
-output_zip = 'output.zip'
-if zipfile.is_zipfile(output_zip):
-    with zipfile.ZipFile(output_zip, 'r') as zip_ref:
-        zip_ref.extractall('output')
---
+++ local/rest_api_gcbm/cml_report/run_gcbm.py
@@ -0,0 +1,29 @@
+"""
+Run the whole simulation
+"""
+
+import os
+from urllib.request import Request
+import requests
+import json
+
+HOME = "http://0.0.0.0:8080/gcbm"
+RESPONSES = []
+
+def run_simulation():
+    url = f"{HOME}/dynamic"
+    req_data = {"title":"run4"}
+    res = requests.post(url,data=req_data)
+    return res
+
+def get_status():
+    url = f"{HOME}/status"
+    req_data = {"title":"run4"}
+    res = requests.post(url,data=req_data)
+    return res.text
+
+if __name__ == "__main__":
+    run_sim = run_simulation()
+    status = {run_sim.text}
+    while(status == {'finished':'In Progress'}):
+        status = get_status()
