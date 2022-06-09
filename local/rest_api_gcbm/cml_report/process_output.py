587b93810bb3aa8487a0426d98dcd537c7eb33fa
radis toubalidis <rtoumpalidis@gmail.com>
2022-06-09 13:32:26 +0300

    check if sim run right

--- local/rest_api_gcbm/cml_report/process_output.py
+++ local/rest_api_gcbm/cml_report/process_output.py
@@ -1,11 +1,6 @@
 import os
 import zipfile
 
-output_zip = os.path.join('..','..','..','output.zip')
+output_zip = os.path.join('output.zip')
 with zipfile.ZipFile(output_zip,'r') as zip_ref:
-    zip_ref.extractall(os.path.join('..','..','..','output'))
-
-output = os.path.join('..','..','..','output')
-print("Output Measurements are:")
-measurements = os.listdir(output)
-print(measurements)
+    zip_ref.extractall('output')
