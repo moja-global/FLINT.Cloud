acd87a8463ba8e33a878abab723929e32962c13d
radis toubalidis <rtoumpalidis@gmail.com>
2022-06-24 12:32:14 +0300

    having issues in dynamic endpoint
    Signed-off-by: radis toubalidis <rtoumpalidis@gmail.com>

--- local/rest_api_gcbm/cml_report/process_input.py
+++ local/rest_api_gcbm/cml_report/process_input.py
@@ -1,6 +1,12 @@
 import zipfile
+import os
 
 #  Unzip file
 zip = 'GCBM_New_Demo_Run.zip'
 with zipfile.ZipFile(zip, 'r') as zip_ref:
     zip_ref.extractall('GCBM')
+
+template = os.path.join('local','rest_api_gcbm','templates.zip')
+extract_to = os.path.join('local','rest_api_gcbm')
+with zipfile.ZipFile(template, 'r') as zip_ref:
+    zip_ref.extractall(extract_to)
