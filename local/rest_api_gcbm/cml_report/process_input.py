75ce8067dfecc5ac7470f24e7686934ce47709b0
radis toubalidis <rtoumpalidis@gmail.com>
2022-06-27 21:58:09 +0300

    Revert "add templates to gitignore"
    This reverts commit 4ed4a3eb1a998b35fd1562f025606c6c9cbe36c8.

--- local/rest_api_gcbm/cml_report/process_input.py
+++ local/rest_api_gcbm/cml_report/process_input.py
@@ -10,3 +10,7 @@ template = os.path.join('local','rest_api_gcbm','templates.zip')
 extract_to = os.path.join('local','rest_api_gcbm')
 with zipfile.ZipFile(template, 'r') as zip_ref:
     zip_ref.extractall(extract_to)
+
+templates_path = os.path.join('local','rest_api_gcbm','templates')
+new_templates_path = os.path.join('local','rest_api_gcbm','template')
+os.rename(templates_path,new_templates_path)
