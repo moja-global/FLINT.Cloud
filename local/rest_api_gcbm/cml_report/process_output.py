be6ae5ae9319f6cf861b7fece3c0d4e9ca13a363
HarshCasper <erbeusgriffincasper@gmail.com>
2022-07-19 09:07:32 +0530

    push to old version

--- local/rest_api_gcbm/cml_report/process_output.py
+++ local/rest_api_gcbm/cml_report/process_output.py
@@ -1,6 +1,6 @@
 import os
 import zipfile
 
-zip = 'test-run.zip'
-with zipfile.ZipFile(zip, 'r') as zip_ref:
-    zip_ref.extractall('output')
+zip = "run4.zip"
+with zipfile.ZipFile(zip, "r") as zip_ref:
+    zip_ref.extractall("output")
