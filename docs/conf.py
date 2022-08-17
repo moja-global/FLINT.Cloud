249c5c46cc0e195cc712d4853363b67a67448ab4
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2022-08-17 16:46:57 +0530

    CI fix
    Signed-off-by: SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@
 
 project = "FLINT.Cloud Technical Guide"
 copyright = "2022, Moja Global"
-author = "Sneha Mishra, Arnav Tiwari & contributors"
+author = u"Sneha Mishra, Arnav Tiwari & contributors"
 
 
 # -- General configuration ---------------------------------------------------
@@ -32,7 +32,6 @@ extensions = [
     "sphinx_rtd_theme",
     "sphinx_copybutton",
     "sphinx_tabs.tabs",
-    "autoapi.extension",
 ]
 
 master_doc = "index"
@@ -51,7 +50,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "furo"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
