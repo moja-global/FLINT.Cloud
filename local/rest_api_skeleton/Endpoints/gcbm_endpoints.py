b93e51800a94ad453ae92676a38a6fe57208bf2b
Freeman <freemanoparaocha@gmail.com>
2022-10-28 00:41:12 +0100

    GCBM Refactor idea Draft
    Signed-off-by: Freeman <freemanoparaocha@gmail.com>

--- local/rest_api_skeleton/Endpoints/gcbm_endpoints.py
+++ local/rest_api_skeleton/Endpoints/gcbm_endpoints.py
@@ -1,13 +1,24 @@
-from flask_restful import Resource 
+from flask_restful import Resource
 import os
 from threading import Thread
-from Helpers.preprocess import DisturbanceConfig, launch_run, ClassifierConfig, MiscellaneousConfig
-from Helpers.for_requests import title_query, miscellaneous_query, disturbance_query, classifier_query
+from Helpers.preprocess import (
+    DisturbanceConfig,
+    launch_run,
+    ClassifierConfig,
+    MiscellaneousConfig,
+)
+from Helpers.for_requests import (
+    title_query,
+    miscellaneous_query,
+    disturbance_query,
+    classifier_query,
+)
+
 
 class disturbance(Resource):
     def post(self):
         title = title_query().get("title") or "simulation"
-        input_dir = f"{os.getcwd()}/input/{title}" 
+        input_dir = f"{os.getcwd()}/input/{title}"
         if not os.path.exists(f"{input_dir}"):
             os.makedirs(f"{input_dir}")
         if not os.path.exists(f"{input_dir}/disturbances"):
@@ -20,12 +31,17 @@ class disturbance(Resource):
         for file in disturbances.get("disturbances"):
             file.save(f"{input_dir}/disturbances/{file.filename}")
             try:
-                disturb = DisturbanceConfig(input_dir, file.filename, disturbances.get("attributes"))
+                disturb = DisturbanceConfig(
+                    input_dir, file.filename, disturbances.get("attributes")
+                )
                 disturb()
-            except Exception as e: 
+            except Exception as e:
                 return e
-        disturb.flatten_directory("disturbances") 
-        return {"data": "Disturbances file uploaded succesfully. Proceed to the next step."}
+        disturb.flatten_directory("disturbances")
+        return {
+            "data": "Disturbances file uploaded succesfully. Proceed to the next step."
+        }
+
 
 class classifier(Resource):
     def post(self):
@@ -44,21 +60,27 @@ class classifier(Resource):
         # store disturbances file in a new folder
         classifiers = classifier_query()
         if not classifiers:
-                return {"error": "Missing classifiers file"}, 400
+            return {"error": "Missing classifiers file"}, 400
 
         for file in classifiers.get("classifiers"):
             file.save(f"{input_dir}/classifiers/{file.filename}")
             try:
-                classify = ClassifierConfig(input_dir, file.filename, classifiers.get("attributes"))
+                classify = ClassifierConfig(
+                    input_dir, file.filename, classifiers.get("attributes")
+                )
                 classify()
             except Exception as e:
                 return e
         classify.flatten_directory("classifiers")
-        return {"data": "Classifiers file uploaded succesfully. Proceed to the next step."}
+        return {
+            "data": "Classifiers file uploaded succesfully. Proceed to the next step."
+        }
+
 
 class Database(Resource):
     def post(self):
         pass
+
 
 class miscellaneous(Resource):
     def post(self):
@@ -77,17 +99,22 @@ class miscellaneous(Resource):
         # store miscellaneous file in a new folder
         mis = miscellaneous_query()
         if not mis:
-                return {"error": "Missing classifiers file"}, 400
+            return {"error": "Missing classifiers file"}, 400
 
         for file in mis.get("miscellaneous"):
             file.save(f"{input_dir}/miscellaneous/{file.filename}")
             try:
-                miscel = MiscellaneousConfig(input_dir, file.filename, mis.get("attributes"))
+                miscel = MiscellaneousConfig(
+                    input_dir, file.filename, mis.get("attributes")
+                )
                 miscel()
             except Exception as e:
                 return e
         miscel.flatten_directory("miscellaneous")
-        return {"data": "Classifiers file uploaded succesfully. Proceed to the next step."}
+        return {
+            "data": "Classifiers file uploaded succesfully. Proceed to the next step."
+        }
+
 
 class title(Resource):
     def post(self):
@@ -105,9 +132,11 @@ class title(Resource):
 
         return {"data": message}, 200
 
+
 class Run(Resource):
     """THIS ENDPOINT WILL BE ABLE TO RUN A SIMULATION
     GOAL IS TO EQUIP IT TO BE ABLE TO RUN MORE THAN ONE SIMULATIONS AT A TIME"""
+
     def post(self):
         title = title_query().get("title") or "simulation"
 
@@ -115,11 +144,12 @@ class Run(Resource):
         title = "".join(c for c in title if c.isalnum())
         input_dir = f"{os.getcwd()}/input/{title}"
 
-
         if not os.path.exists(f"{input_dir}"):
             os.makedirs(f"{input_dir}")
 
-        thread = Thread(target=launch_run, kwargs={"title": title, "input_dir": input_dir})
+        thread = Thread(
+            target=launch_run, kwargs={"title": title, "input_dir": input_dir}
+        )
         thread.start()
 
         return {"status": "Run started"}, 200
