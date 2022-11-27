7a561fe54824dbf2c9fa5f9b0a5079a3def63221
Freeman <freemanoparaocha@gmail.com>
2022-11-27 14:14:38 +0100

    added MVC architecture

--- local/rest_api_skeleton/app.py
+++ local/rest_api_skeleton/app.py
@@ -1,16 +1,13 @@
 from flask import Flask
 from flask_restful import Api
-from Endpoints.gcbm_endpoints import disturbance, Run, title, classifier, miscellaneous
+from Helpers.routes import endpoints
+
 
 app = Flask(__name__)
 api = Api()
 
-api.add_resource(disturbance, "/gcbm/upload/disturbances")
-api.add_resource(title, "/gcbm/create")
-api.add_resource(classifier, "/gcbm/upload/classifies")
-api.add_resource(miscellaneous, "/gcbm/upload/miscellaneous")
-api.add_resource(Run, "/gcbm/run")
 
+endpoints(api=api)
 
 if __name__ == "__main__":
     app.run(debug=True)
